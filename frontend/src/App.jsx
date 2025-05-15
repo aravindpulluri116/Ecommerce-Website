c0a6e5f3b3b4d76004c03f04f9fb06cd71e344dc
aravindpulluri116 <pulluriaravind@gmail.com>
2025-05-16 00:37:02 +0530

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import {Routes,Route} from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import Collection from './pages/Collection.jsx'
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Product from './pages/Product';
+import Cart from './pages/Cart';
+import Login from './pages/Login';
+import PlaceOrder from './pages/PlaceOrder';
+import Orders from './pages/Orders';
+import Navbar from './components/Navbar.jsx';
+import Footer from './components/Footer.jsx';
+
+const App = () => {
+  return (
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/collection' element={<Collection/>} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/contact' element={<Contact/>} />
+        <Route path='/product' element={<Product/>} />
+        <Route path='/product/:productid' element={<Product/>} />
+        <Route path='/cart' element={<Cart/>} />
+        <Route path='/login' element={<Login/>} />
+        <Route path='/place-order' element={<PlaceOrder/>} />
+        <Route path='/order' element={<Orders/>} />
+
+ 
+
+      </Routes>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/components/NewsletterBox.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const NewsletterBox = () => {
+  const onSubmitHandler=()=>{
+      event.preventDefault()
+
+  }
+  return (
+    <div className='text-center '>
+        <p className='text-2xl font medium text-gray-800 '>Subscribe Now & get 20% off</p>
+        <p className='text-gray-400 mt-3' >
+        </p>
+        <form onSubmit={onSubmitHandler} className='w-full sm:w-1/2 flex items-center gap-3 mx-auto my-6 border pl-3 rounded-4xl'>
+          <input className='w-full sm:flex-1 outline-none' type='email' placeholder="Enter your email" />
+          <button type="submit" className='bg-black text-white text-xs border-4 rounded-4xl px-10 py-4'>Subscribe</button>
+        </form>   
+    </div>
+  );
+}
+
+export default NewsletterBox;
