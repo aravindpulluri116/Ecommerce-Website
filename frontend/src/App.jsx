c2c9d48d8fd6d70f0c1d81a93f69b96638100075
aravindpulluri116 <pulluriaravind@gmail.com>
2025-06-05 22:46:49 +0530

    updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,18 +11,23 @@ import PlaceOrder from './pages/PlaceOrder';
 import Orders from './pages/Orders';
 import Navbar from './components/Navbar.jsx';
 import Footer from './components/Footer.jsx';
+import SearchBar from './components/SearchBar.jsx';
+import { ToastContainer, toast } from 'react-toastify';
+
 
 const App = () => {
   return (
     <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <ToastContainer  />
       <Navbar/>
+      <SearchBar/>
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/collection' element={<Collection/>} />
         <Route path='/about' element={<About/>} />
         <Route path='/contact' element={<Contact/>} />
         <Route path='/product' element={<Product/>} />
-        <Route path='/product/:productid' element={<Product/>} />
+        <Route path='/product/:productID' element={<Product/>} />
         <Route path='/cart' element={<Cart/>} />
         <Route path='/login' element={<Login/>} />
         <Route path='/place-order' element={<PlaceOrder/>} />
--- frontend/src/pages/Orders.jsx
+++ frontend/src/pages/Orders.jsx
@@ -1,8 +1,24 @@
 import React from 'react';
+import { ShopContext } from '../context/ShopContext';
+import Title from '../components/Title';
 
 const Orders = () => {
+  const {currency,products}=useContext(ShopContext)
   return (
-    <div>
+    <div className='border-t pt-16' >
+      <div className='text-2xl' >
+        <Title text1={"MY"} text2={" ORDERS"} />
+      </div>
+      <div>
+        {
+          products.slice(1,4).map((item,index)=>(
+            <div>
+              
+            </div>
+            
+          ))
+        }
+      </div>
       
     </div>
   );
