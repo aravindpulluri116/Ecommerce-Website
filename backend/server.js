f54c9fdd11b1301faa7e11d762a0faf4f91f685b
aravindpulluri116 <pulluriaravind@gmail.com>
2025-07-11 15:36:23 +0530

    cloudinary

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,27 @@
-import express from 'express'
-import cors from 'cors'
-import 'dotenv/config' 
-import { connect } from 'mongoose'
-import connectDB from './config/mongodb.js'
-import pkg from '@eslint/js';
-import connectCloudinary from './config/cloudinary.js';
-const {js} =pkg;
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import { connect } from "mongoose";
+import connectDB from "./config/mongodb.js";
+import pkg from "@eslint/js";
+import connectCloudinary from "./config/cloudinary.js";
+const { js } = pkg;
 
 //App config
-const app=express()
-const port=process.env.PORT || 4000
-connectDB()
-connect(connectCloudinary)
+const app = express();
+const port = process.env.PORT || 4000;
+connectDB();
+connectCloudinary();
 
 // middleware
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
 //api
-app.get('/',(req,res)=>{
-    res.send("API Working")
-})
+app.get("/", (req, res) => {
+  res.send("API Working");
+});
 
-app.listen(port,()=>{console.log("Server started on PORT: "+port)})
-
+app.listen(port, () => {
+  console.log("Server started on PORT: " + port);
+});
